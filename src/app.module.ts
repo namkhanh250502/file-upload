517f98d9c4a627be93c6e221a9a9ba3b7343e34b
Khanh <namkhanh250502@gmail.com>
2023-09-07 09:23:57 +0700

    prismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaService } from './prisma.service';
+
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
